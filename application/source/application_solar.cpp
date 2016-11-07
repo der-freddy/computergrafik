d0c39ce0fede19e75506e1def602f1ea291af531
Hans Lienhop <tuje8374@billl09.in.uni-weimar.de>
2016-11-07 13:58:57 +0100

    merge

--- application/source/application_solar.cpp
+++ application/source/application_solar.cpp
@@ -163,6 +163,8 @@ void ApplicationSolar::initializeShaderPrograms() {
 	m_shaders.at("planet").u_locs["ModelMatrix"] = -1;
 	m_shaders.at("planet").u_locs["ViewMatrix"] = -1;
 	m_shaders.at("planet").u_locs["ProjectionMatrix"] = -1;
+	m_shaders.at("planet").u_locs["PlanetColor"] = -1;
+
 
 	m_shaders.emplace("star", shader_program{m_resource_path + "shaders/star.vert",
 																					 m_resource_path + "shaders/star.frag"});
--- resources/shaders/simple.frag
+++ resources/shaders/simple.frag
@@ -4,5 +4,5 @@ in  vec3 pass_Normal;
 out vec4 out_Color;
 
 void main() {
-  out_Color = pass_Normal;
+  out_Color = vec4(pass_Normal, 1.0);
 }
